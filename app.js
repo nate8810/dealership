950122f49797a6978c53352af51e05bd07a278cc
Nate <sneeky8806@yahoo.com>
2017-12-04 19:38:16 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName('slides');
+  var dots = document.getElementsByClassName('dot')
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    dots[i].className = dot[i].className.replace("active", "")
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className +=".active";
+}
